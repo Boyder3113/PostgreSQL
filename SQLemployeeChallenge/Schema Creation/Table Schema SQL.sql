79c807ffb8459ed0f3bf7e42a83dbfba039f60ef
Boyd Evans III <bbeiii@yahoo.com>
2021-05-05 11:31:52 -0600

    adding dept_emp table

--- SQLemployeeChallenge/Schema Creation/Table Schema SQL.sql
+++ SQLemployeeChallenge/Schema Creation/Table Schema SQL.sql
@@ -16,3 +16,11 @@ create table employees (
 	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no)
 );
+
+create table dept_emp(
+	emp_no varchar NOT NULL,
+	dept_no varchar NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY(emp_no, dept_no)
+);
