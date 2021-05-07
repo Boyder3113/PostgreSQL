96389266cd87a1cafbe5bfd011e4deb4381b5661
Boyd Evans III <bbeiii@yahoo.com>
2021-05-07 14:36:05 -0600

    adding 2nd query

--- SQLemployeeChallenge/Schema Creation/SQL Queries.sql
+++ SQLemployeeChallenge/Schema Creation/SQL Queries.sql
@@ -11,3 +11,10 @@ Select e.emp_no,
 FROM employees as e
 Join salaries as s
 ON (e.emp_no = s.emp_no);
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+
+Select first_name, last_name, hire_date
+From employees
+Where hire_date between '1986-01-01' and '1986-12-31'
+Order By hire_date;
